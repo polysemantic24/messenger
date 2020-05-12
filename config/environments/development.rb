86d5d115753363127e098496b358d42a0a57b5fa
polysemantic24 <pavel@arttaganka.ru>
2020-05-12 13:55:55 +0300

    deploy app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.allowed_request_origins = ['https://fierce-springs-41077.herokuapp.com']
+  # config.action_cable.allowed_request_origins = ['https://fierce-springs-41077.herokuapp.com']
 end
